b07da75dd4180b6843f8a3ea008b9493a85aa85c
muhadzdzib al khairy <113493493+muhadzdzib-al-khairy@users.noreply.github.com>
2022-11-02 14:59:15 +0700

    Add files via upload

---
+++ latihan-service/src/test/java/com/tkb_muhadzdzib_Al_khairy/latihan/service/LatihanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkb_muhadzdzib_Al_khairy.latihan.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatihanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
